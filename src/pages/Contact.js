d645a497d6f5614401174408be4e14c9773812eb
SophieHeb <sophie.hebdidge@gmail.com>
2020-09-19 20:27:53 +0100

    Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -21,50 +21,52 @@ function Contact() {
      <div 
       className={`container-fluid position-relative p-5 mt-5 d-flex align-items-center`}
     >
-      <div className="col-6">
-        <h2 className="text-center">Contact</h2>
-        <h1 className="text-center">Let's have a chat</h1>
-        <div className="row d-flex align-items-center justify-content-center">
-        <div className='col-6 p-5 '>
-          <h2 className="text-center">Media Enquiries</h2>
-          <p className="text-center">press@techniclarity.co</p>
-          <div className="text-center">
-            <a href="mailto:press@techniclarity.co">
-            <MailIcon size={36}/>
-            </a>
+      <div className="row">
+        <div className="col-lg-6 col-12">
+          <h2 className="text-center">Contact</h2>
+          <h1 className="text-center">Let's have a chat</h1>
+          <div className="row d-flex align-items-center justify-content-center">
+          <div className='col-sm-6 col-12 p-5 '>
+            <h2 className="text-center">Media Enquiries</h2>
+            <p className="text-center">press@techniclarity.co</p>
+            <div className="text-center">
+              <a href="mailto:press@techniclarity.co">
+              <MailIcon size={36}/>
+              </a>
+            </div>
+          </div>
+          <div className='col-sm-6 col-12 p-5 '>
+            <h2 className="text-center">Work together</h2>
+            <p className="text-center">partnerships@techniclarity.co</p>
+            <div className="text-center">
+              <a href="mailto:partnerships@techniclarity.co">
+              <MailIcon size={36}/>
+              </a>
+            </div>
+          </div>
+          <div className='col-sm-6 col-12 p-5 '>
+            <h2 className="text-center">Everything else</h2>
+            <p className="text-center">hello@techniclarity.co</p>
+            <div className="text-center">
+              <a href="mailto:hello@techniclarity.co">
+              <MailIcon size={36}/>
+              </a>
+            </div>
           </div>
         </div>
-        <div className='col-6 p-5 '>
-          <h2 className="text-center">Work together</h2>
-          <p className="text-center">partnerships@techniclarity.co</p>
-          <div className="text-center">
-            <a href="mailto:partnerships@techniclarity.co">
-            <MailIcon size={36}/>
-            </a>
-          </div>
         </div>
-        <div className='col-6 p-5 '>
-          <h2 className="text-center">Everything else</h2>
-          <p className="text-center">hello@techniclarity.co</p>
-          <div className="text-center">
-            <a href="mailto:hello@techniclarity.co">
-            <MailIcon size={36}/>
-            </a>
-          </div>
+        <div className="col-lg-6 col-12 h-100 position-relative d-flex justify-content-center align-items-center">
+          <img
+          src={greenDiamond}
+          alt="Green diamond background"
+          className={`w-100 `}
+          />
+          <img
+            src={woman}
+            alt="Line drawing of a woman"
+            className={`h-100 position-absolute`}
+          />
         </div>
-      </div>
-      </div>
-      <div className="col-6 h-100 position-relative d-flex justify-content-center align-items-center">
-        <img
-        src={greenDiamond}
-        alt="Green diamond background"
-        className={`w-100 `}
-        />
-        <img
-          src={woman}
-          alt="Line drawing of a woman"
-          className={`h-100 position-absolute`}
-        />
       </div>
     </div>
 
